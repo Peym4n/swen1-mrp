34441e26fb484729bbcf61e3107796cf58084259
Peyman Aparviz <peyman.aparviz@gmail.com>
2025-12-12 00:56:54 +0100

    main: start server

--- src/main/java/at/fhtw/swen1/mrp/Main.java
+++ src/main/java/at/fhtw/swen1/mrp/Main.java
@@ -1,9 +1,24 @@
 package at.fhtw.swen1.mrp;
 
+import at.fhtw.swen1.mrp.handler.MediaHandler;
+import at.fhtw.swen1.mrp.service.MediaService;
 import at.fhtw.swen1.mrp.data.DatabaseConnection;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DatabaseConnection.executeInitScript();
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        MediaService mediaService = new MediaService();
+        server.createContext("/api/media", new MediaHandler(mediaService));
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on port " + port);
     }
 }
