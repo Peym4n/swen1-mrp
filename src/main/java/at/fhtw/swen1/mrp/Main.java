1a8943c0a4ce3e740e6a8140cecb5f1a748fb461
Peyman Aparviz <peyman.aparviz@gmail.com>
2025-12-16 10:36:53 +0100

    finish main

--- src/main/java/at/fhtw/swen1/mrp/Main.java
+++ src/main/java/at/fhtw/swen1/mrp/Main.java
@@ -1,8 +1,12 @@
 package at.fhtw.swen1.mrp;
 
+import at.fhtw.swen1.mrp.data.DatabaseManager;
 import at.fhtw.swen1.mrp.handler.MediaHandler;
+import at.fhtw.swen1.mrp.handler.UserHandler;
+import at.fhtw.swen1.mrp.repository.MediaRepository;
+import at.fhtw.swen1.mrp.repository.UserRepository;
 import at.fhtw.swen1.mrp.service.MediaService;
-import at.fhtw.swen1.mrp.data.DatabaseConnection;
+import at.fhtw.swen1.mrp.service.UserService;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
@@ -10,12 +14,25 @@ import java.net.InetSocketAddress;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        DatabaseConnection.executeInitScript();
+        // Initialize DB
+        DatabaseManager.getInstance();
+
         int port = 8080;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
-        MediaService mediaService = new MediaService();
-        server.createContext("/api/media", new MediaHandler(mediaService));
+        // Wiring
+        UserRepository userRepository = new UserRepository();
+        UserService userService = new UserService(userRepository);
+        UserHandler userHandler = new UserHandler(userService);
+
+        MediaRepository mediaRepository = new MediaRepository();
+        MediaService mediaService = new MediaService(mediaRepository);
+        MediaHandler mediaHandler = new MediaHandler(mediaService, userService);
+
+        // Contexts
+        server.createContext("/api/users/register", userHandler);
+        server.createContext("/api/users/login", userHandler);
+        server.createContext("/api/media", mediaHandler);
 
         server.setExecutor(null);
         server.start();
